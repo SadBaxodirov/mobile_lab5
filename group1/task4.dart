d489eaa4dc70ec20c26b8002a22744169e1612a6
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2025-09-24 09:57:40 +0500

    Create task4.dart

---
+++ group1/task4.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.redAccent,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        height: 150,
+        width: 100,
+        child: const Icon(Icons.home),
+      ),
+    )));
+  }
+}
