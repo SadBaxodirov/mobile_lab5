d78668065d97f4d0828c14435e8429cecb15afa7
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2025-09-24 10:28:51 +0500

    Create task1.dart

---
+++ group3/task1.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+      child: Container(
+          padding: const EdgeInsets.all(16),
+          //margin: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: 300,
+          width: 350,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "enter your password",
+            ),
+          )),
+    )));
+  }
+}
