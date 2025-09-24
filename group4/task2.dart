b42963d8ce5333ed767525a85d980c920ce0b9e6
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2025-09-24 10:50:43 +0500

    Create task2.dart

---
+++ group4/task2.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final TextEditingController controller = TextEditingController();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+                child: Container(
+      padding: const EdgeInsets.all(16),
+      //margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.redAccent,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: 300,
+      width: 350,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(height: 100, color: Colors.red),
+            flex: 1,
+          ),
+          Expanded(
+            child: Container(height: 100, color: Colors.green),
+            flex: 2,
+          )
+        ],
+      ),
+    ))));
+  }
+}
